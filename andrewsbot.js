36305e9687257a48e211f19f051e7a96806214af
Andrew <42401261+andrewL1234@users.noreply.github.com>
2019-01-19 22:20:55 -0500

    Update andrewsbot.js

--- andrewsbot.js
+++ andrewsbot.js
@@ -13,30 +13,30 @@ client.on('ready', () => {
  
 
 client.on('message', message => {
-    const commandArray = message.split(' ');
-    const command = commandArray[0];
-    const args = commandArray.slice(1);
-    if (command === 'random') {
+//     const commandArray = message.split(' ');
+//     const command = commandArray[0];
+//     const args = commandArray.slice(1);
+//     if (command === 'random') {
         
-        if(args.length !== 2) {
-            return message.channel.send("Please enter TWO numbers after the command.");
+//         if(args.length !== 2) {
+//             return message.channel.send("Please enter TWO numbers after the command.");
 
-        }
-        const minimum = Number(args[0]);
-        const maximum = Number(args[1]);
-        else if(isNaN(minimum) || isNaN(maximum)) {
-            return message.channel.send("Please enter two NUMBERS after the command.");
+//         }
+//         const minimum = Number(args[0]);
+//         const maximum = Number(args[1]);
+//         else if(isNaN(minimum) || isNaN(maximum)) {
+//             return message.channel.send("Please enter two NUMBERS after the command.");
 
-        }
-        else if(minimum >= maximum) {
-            return message.channel.send("Please enter the minumum first and then the maximum.");
+//         }
+//         else if(minimum >= maximum) {
+//             return message.channel.send("Please enter the minumum first and then the maximum.");
 
-        } else {
-            const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
-            message.channel.send(randomNumber);
-        }
-    } 
-    else if(command === 'ping') {
+//         } else {
+//             const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
+//             message.channel.send(randomNumber);
+//         }
+//     } 
+    if(command === 'ping') {
         return message.reply('pong');
     }
     
