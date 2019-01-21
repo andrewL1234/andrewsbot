21b3fe16bb1738ed1da4a5a42763fbcae4bbe572
Andrew <42401261+andrewL1234@users.noreply.github.com>
2019-01-21 11:42:03 -0500

    Update andrewsbot.js

--- andrewsbot.js
+++ andrewsbot.js
@@ -15,27 +15,27 @@ client.on('ready', () => {
 client.on('message', message => {
        const commandArray = message.content.split(' ');
        const command = commandArray[0];
-//     const args = commandArray.slice(1);
-//     if (command === 'random') {
-        
-//         if(args.length !== 2) {
-//             return message.channel.send("Please enter TWO numbers after the command.");
+       const args = commandArray.slice(1);
+       if (command === 'random') {
 
-//         }
-//         const minimum = Number(args[0]);
-//         const maximum = Number(args[1]);
-//         else if(isNaN(minimum) || isNaN(maximum)) {
-//             return message.channel.send("Please enter two NUMBERS after the command.");
+           if(args.length !== 2) {
+               return message.channel.send("Please enter TWO numbers after the command.");
 
-//         }
-//         else if(minimum >= maximum) {
-//             return message.channel.send("Please enter the minumum first and then the maximum.");
+           }
+           const minimum = Number(args[0]);
+           const maximum = Number(args[1]);
+           else if(isNaN(minimum) || isNaN(maximum)) {
+               return message.channel.send("Please enter two NUMBERS after the command.");
 
-//         } else {
-//             const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
-//             message.channel.send(randomNumber);
-//         }
-//     } 
+           }
+           else if(minimum >= maximum) {
+               return message.channel.send("Please enter the minumum first and then the maximum.");
+
+           } else {
+               const randomNumber = Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
+               message.channel.send(randomNumber);
+           }
+       } 
     if(command === 'ping') {
         return message.reply('pong');
     }
